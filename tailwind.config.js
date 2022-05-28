1369b7ab65354a293a618299e415f98cb811ad08
ILhankhondaker <ilhankhondaker@gmail.com>
2022-05-28 15:31:20 +0600

    change

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,4 +20,26 @@ module.exports = {
   },
   plugins: [require("daisyui")],
 }
+// module.exports = {
+//   content: ["./src/**/*.{html,js}"],
+//   theme: {
+//     extend: {},
+//   },
+//   daisyui: {
+//     themes: [
+//       {
+//         doctortheme: {
+//           primary: "#0FCFEC",
+//           secondary: "#19D3AE",
+//           accent: "#3A4256",
+//           neutral: "#3d4451",
+//           "base-100": "#ffffff",
+//         },
+//       },
+//       "dark",
+//       "cupcake",
+//     ],
+//   },
+//   plugins: [require("daisyui")],
+// }
 
