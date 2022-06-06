8c25241a2262d37df07d878316e3ae0cf437ffea
ILhankhondaker <ilhankhondaker@gmail.com>
2022-06-06 18:59:39 +0600

    done

--- src/Pages/Dashboard/Portfolio.js
+++ src/Pages/Dashboard/Portfolio.js
@@ -19,9 +19,10 @@ const Portfolio = () => {
                     <a href="https://twitter.com/IlhanKhondaker" class="button" target='blank'><BsTwitter /> </a>
                     <a href="https://www.google.com/search?gs_ssp=eJzj4tVP1zc0zI3PMS1Irsw2YLRSNagwNjc1NrM0SLJMMjaxMEwytTKoSExJNE02SE0yMTVNSUkyNvTiz8zJSMxTyM7Iz0tJzE4tAgDYCxVv&q=ilhan+khondaker&rlz=1C1CHBF_enBD886BD886&oq=Ilhan+kh&aqs=chrome.1.69i57j46i39i175i199j0i512l2j0i10i22i30j0i22i30l2j69i59.3052j0j7&sourceid=chrome&ie=UTF-8" class="button" target='blank'><BsGoogle /></a>
 
-                    <a href="https://www.linkedin.com/in/ilhan-khondaker-9a46b01b5/"><BsLinkedin></BsLinkedin></a>
-
+                    <a href="https://www.linkedin.com/in/ilhan-khondaker-9a46b01b5/"><BsLinkedin></BsLinkedin></a> <br />
+                    (please check my Google info)
                 </h1>
+                <h1>My Some project : <a className='font-bold' href="https://ass-11-118c8.web.app/?fbclid=IwAR2DeO2P9Wny77M5lJbFY4RMjf1pxauvT25stHyAwclIkglrTcPy_-ixJU8" target='blank'>Ride Car House</a></h1>
             </div>
             <div className='flex justify-between hidden lg:block p-10 '>
                 <img className='rounded-full w-56 ' src="https://scontent.fdac24-2.fna.fbcdn.net/v/t1.6435-9/195518759_777353176314051_2441569181417949456_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGEvqCA6Y0JUbznegYTx0OWvJsidAUA-8C8myJ0BQD7wNUrt6tiE5oQrYFypo5WLpQyt628C3-F2O8KpYwvcM14&_nc_ohc=6G-Iaoo6bfUAX9_0iHg&_nc_ht=scontent.fdac24-2.fna&oh=00_AT-fScPlt52pNjz70-j4LLCaKOq7QHowLAyFlj3rqmQkMQ&oe=62B67493" alt="" />
